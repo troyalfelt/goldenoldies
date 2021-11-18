408813892727dfe8d6f7b1edac19442dc71a476c
Michael Baker <michaelbaker117@gmail.com>
2021-11-18 07:32:05 -0500

    updated pages

--- login.php
+++ login.php
@@ -17,11 +17,28 @@
       background-color: #7fffd4;
       width: 100%;
     }
+    form {
+      background-color: #7fffd4;
+      border-radius: 5px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      width: 400px;
+    }
     footer {
       background-color: #C0C0C0;
       display: flex;
       flex-direction: column;
       align-items: center;
+      width: 100%;
+    }
+    .login {
+      background-color: #f0ffff;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: space-around;
+      height: 590px;
       width: 100%;
     }
   </style>
@@ -31,8 +48,14 @@
       <h1>Golden Oldies</h1>
   </header>
 <div class="login">
-  <form>
-    
+  <form action="" method="post">
+    <h1>Login</h1>
+    <hr>
+    <label for="email"><b>Email</b></label><br>
+    <input id="email" name="email" type="text" placeholder="Enter Your Email" class="textbox"/></br></br>
+    <label for="password"><b>Password</b></label><br>
+    <input id="password" name="password" type="password" placeholder="Enter Your Password" class="textbox"/></br></br>
+    <input type="submit" class="btn" value="Sign In"></br></br>
   </form>
   <div>
 
--- register.php
+++ register.php
@@ -17,6 +17,14 @@
       background-color: #7fffd4;
       width: 100%;
     }
+    form {
+      background-color: #7fffd4;
+      border-radius: 5px;
+      display: flex;
+      justify-content: space-around;
+      height: 550px;
+      width: 700px;
+    }
     footer {
       background-color: #C0C0C0;
       display: flex;
@@ -24,18 +32,67 @@
       align-items: center;
       width: 100%;
     }
+    .register {
+      align-items: center;
+      background-color: #f0ffff;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      height: 590px;
+      width: 100%;
+    }
+    .user {
+      display: flex;
+      flex-direction: column;
+    }
+    .patient {
+      display: flex;
+      flex-direction: column;
+      align-self: center;
+    }
   </style>
 </head>
 <body>
   <header>
       <h1>Golden Oldies</h1>
   </header>
-<div class="register">
-  <form>
-    
-  </form>
-  <div>
-      <a href="login.php">Okay</a><br>
+  <div class="register">
+    <form action="" method="post">
+      <div class="user">
+        <h1>Create New User</h1>
+        <label for="role"><b>Select Role</b></label>
+        <select id="role" name="role">
+          <option value="admin">Admin</option>
+          <option value="supervisor">Supervisor</option>
+          <option value="doctor">Doctor</option>
+          <option value="careGiver">Care Giver</option>
+          <option value="patient">Patient</option>
+          <option value="family">Family member</option>
+        </select><br>
+        <label for="fName"><b>First Name</b></label>
+        <input type="text" placeholder="Enter First Name" name="fName" id="fName" required><br>
+        <label for="lName"><b>Last Name</b></label>
+        <input type="text" placeholder="Enter Last Name" name="lName" id="lName" required><br>
+        <label for="email"><b>Email</b></label>
+        <input type="email" placeholder="Enter Email" name="email" id="email" required><br>
+        <label for="phone"><b>Phone Number</b></label>
+        <input type="text" placeholder="Enter Phone Number" name="phone" id="phone" required><br>
+        <label for="password"><b>Password</b></label>
+        <input type="password" placeholder="Password" name="password" id="password" required><br>
+      </div>
+      <div class="patient">
+          <h2>For Patient</h2>
+          <label for="dob"><b>Date of Birth</b></label>
+          <input type="date" name="dob" id="dob"><br>
+          <label for="fCode"><b>Family Code</b></label>
+          <input type="text" placeholder="Enter Code" name="fCode" id="fCode"><br>
+          <label for="eContact"><b>Emergency Contact</b></label>
+          <input type="text" placeholder="Enter Contact" name="eContact" id="eContact"><br>
+          <label for="relation"><b>Relation to Emergency Contact</b></label>
+          <input type="text" placeholder="Enter Relation" name="relation" id="relation"><br>
+          <input type="submit" class="btn" name="submit" value="Register">
+      </div>
+    </form>
   </div>
 </div>
 <footer>
