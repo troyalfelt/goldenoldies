8bb387a77f91d2ba20de9d8dafa99ff2db73227e
Michael Baker <michaelbaker117@gmail.com>
2021-11-19 09:58:44 -0500

    approavl page prototype

--- admin/approval.php
+++ admin/approval.php
@@ -9,14 +9,36 @@
   <header>
       <h1>Golden Oldies</h1>
   </header>
-<div class="container">
-  <div>
-
+  <div class="container">
+  <h2>Registration Approval</h2>
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Role</th>
+        <th>Approve</th>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td>
+          <label for="yes"><b>Yes</b></label>
+          <input type="checkbox" name="yes">
+          <label for="no"><b>No</b></label>
+          <input type="checkbox" name="no"><br>
+        </td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+      </tr>
+    </table>
+    <form action="" method="post">
+      <input type="submit" class="btn" name="submit" value="Okay">
+    </form>
   </div>
-</div>
-<footer>
-    <h3>Contact Us</h3>
-    <p>000-000-0000</p>
-</footer>
+  <footer>
+      <h3>Contact Us</h3>
+      <p>000-000-0000</p>
+  </footer>
 </body>
 </html>
