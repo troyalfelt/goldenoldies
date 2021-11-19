3bc9134526c007ebac491b030fe70ae25a52582c
Aleksander Carr <aleks.carr@outlook.com>
2021-11-19 10:26:42 -0500

    got the approval page working

--- admin/approval.php
+++ admin/approval.php
@@ -3,7 +3,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
   ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,7 +10,6 @@ error_reporting(E_ALL);
   <title>Approval</title>
   <link rel="stylesheet" href="../styles.css">
 </head>
-
 <?php
 $servername = "localhost";
 $username = "troyalfelt";
@@ -36,52 +34,54 @@ $conn = new mysqli($servername, $username, $password, $db);
       <h1>Golden Oldies</h1>
   </header>
   <div class="container">
-      <table>
-        <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Role</th>
-            <th>Approval</th>
+  <h2>Registration Approval</h2>
+  <table>
+    <tr>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Role</th>
+        <th>Approval</th>
+    </tr>
+    <?php
+    $servername = "localhost";
+    $username = "troyalfelt";
+    $password = "";
+    $db = "test";
+
+     //Create connection
+    $conn = mysqli_connect($servername, $username, $password, $db);
+     //Check connection
+    if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+    }
+
+    $sql = "SELECT fName, lName, role_name FROM user";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+    echo "<tr><td>". $row["fName"]. "</td><td>" . $row["lName"]. "</td><td>". $row["role_name"]. "</td></tr>";
+    }
+    }
+    $conn->close();
+    ?>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        td><form action="" method="post" class="">Yes
+          <input type="checkbox" name="approved" value="1"/>No
+            <input type="checkbox" name="approved" value="0"/>
+        </td>
         </tr>
-<?php
-$servername = "localhost";
-$username = "troyalfelt";
-$password = "";
-$db = "test";
-
- //Create connection
-$conn = mysqli_connect($servername, $username, $password, $db);
- //Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
-
-$sql = "SELECT fName, lName, role_name FROM user";
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-// output data of each row
-while($row = $result->fetch_assoc()) {
-echo "<tr><td>". $row["fName"]. "</td><td>" . $row["lName"]. "</td><td>". $row["role_name"]. "</td></tr>";
-}
-}
-$conn->close();
-?>
-<td></td>
-<td></td>
-<td></td>
-<td><form action="" method="post" class="">Yes
-  <input type="checkbox" name="approved" value="1"/>No
-    <input type="checkbox" name="approved" value="0"/>
-</td>
-</tr>
-</table>
-    <input type="submit" class="btn" name="submit" value="Okay">
-    <input type="reset" class="btn" name="submit" value="Cancel">
-  </form>
-</div>
-<footer>
-    <h3>Contact Us</h3>
-    <p>000-000-0000</p>
-</footer>
-</body>
-</html>
+        </table>
+            <input type="submit" class="btn" name="submit" value="Okay">
+            <input type="reset" class="btn" name="submit" value="Cancel">
+          </form>
+        </div>
+        <footer>
+            <h3>Contact Us</h3>
+            <p>000-000-0000</p>
+        </footer>
+        </body>
+        </html>
