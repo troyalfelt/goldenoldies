ed6c7c67758458e1f723ce381b11a796c4732d89
Troy Alfelt <troyalfelt@Troys-MBP.fios-router.home>
2021-11-27 11:34:01 -0500

    approval page now lets you delete unwanted registrants

--- admin/approval.php
+++ admin/approval.php
@@ -7,8 +7,6 @@ if (!isset($_SESSION['access_lvl'])) {
     header("Location: ../login.php");
 }
 }
-
-echo $_SESSION['access_lvl'];
 ?>
 
 
@@ -39,58 +37,56 @@ $conn = new mysqli($servername, $username, $password, $db);
         <th>First Name</th>
         <th>Last Name</th>
         <th>Role</th>
-        <th>Approved</th>
     </tr>
   <?php
   if (isset($_POST['submit']))  {
-        $approved = $_POST['approved'];
+        $user_id = $_POST['user_id'];
 
-   $sql = "UPDATE user SET approved='1' WHERE approved=$approved";
-
-   if ($conn->query($sql) === TRUE) {
-    echo "Record updated successfully";
-   } else {
-    echo "Error updating record: " . $conn->error;
-   }
-
-   $conn->close();
+        if ($_POST['status'] == 'approved') {
+          $qry = "UPDATE user SET approved = 1 WHERE user_id= '$user_id'";
+          $rslt = $conn->query($qry);
+          echo 'User approved';
+          } else {
+            $qry = "DELETE FROM user WHERE user_id= '$user_id'";
+            $rslt = $conn->query($qry);
+            if ($rslt == TRUE) {
+              echo "User deleted";
+            } else {
+                echo "Error deleting record: " . $conn->error;
+            }
+          }
 }
 ?>
 <?php
-    $servername = "localhost";
-    $username = "troyalfelt";
-    $password = "";
-    $db = "test";
 
-     //Create connection
-    $conn = mysqli_connect($servername, $username, $password, $db);
-     //Check connection
-    if (!$conn) {
-      die("Connection failed: " . mysqli_connect_error());
-    }
-
-    $sql = "SELECT fname, lname, role_name FROM user WHERE approved = 0 ";
+    $sql = "SELECT user_id, fname, lname, role_name FROM user WHERE approved = 0 ";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-    echo "<tr><td>". $row["fname"]. "</td><td>" . $row["lname"]. "</td><td>". $row["role_name"]. "</td><td><input type='checkbox' name='approved' value='".$row["approved"]."' >";
-}
-    }
-
-    $conn->close();
+      ?><tr>
+            <?php echo "<td>" . $row["fname"] . "</td>"?>
+            <?php echo "<td>" . $row["lname"] . "</td>"?>
+            <?php echo "<td>" . $row["role_name"] . "</td>"?>
+            <td>
+              <form action='' method='post' name='approved'>
+                <?php echo"<input type='hidden' name='user_id' value=" . $row['user_id'] . ">";?>
+                <input type='hidden' name='status' value='approved'>
+                <input type='submit' name='submit' value='Approve'>
+                </form>
+            </td>
+            <td>
+              <form action='' method='post' name='disapproved'>
+                <?php echo"<input type='hidden' name='user_id' value=" . $row['user_id'] . ">";?>
+                <input type='hidden' name='status' value='disapproved'>
+              <input type='submit' name='submit' value='Disapprove'>
+              </form>
+            </td>
+          </tr>
+<?php }
+  }
   ?>
-      <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-          <form method = "post" action = "">
-                <input type="hidden" name="approved" value="approved">
-        </tr>
-        </table>
-            <input type="submit" class="btn" name="submit" value="Okay">
-          </form>
         </div>
         <footer>
         <script>  if ( window.history.replaceState ) {
