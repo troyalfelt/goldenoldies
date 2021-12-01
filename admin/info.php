0cb86b6c20a6469888967cd9a9aff767f10f4b95
Troy Alfelt <troyalfelt@Troys-MBP.fios-router.home>
2021-12-01 12:49:54 -0500

    new roster can now be created, extra patient info added

--- admin/info.php
+++ admin/info.php
@@ -8,5 +8,74 @@ if (!isset($_SESSION['access_lvl'])) {
 }
 }
 ?>
+<?php
+$servername = "localhost";
+$username = "troyalfelt";
+$password = "";
+$db = 'test';
+$conn = new mysqli($servername, $username, $password, $db);
+ ?>
+ <?php
+   if (isset($_POST['submit'])) {
+     $user_id = $_POST['user_id'];
+     $group_number = $_POST['group_number'];
+     $admit_date = $_POST['admit_date'];
+     $qry = "UPDATE patient SET group_number='$group_number', admit_date='$admit_date' WHERE user_id = '$user_id'";
+     $rslt = $conn->query($qry);
+   }?>
+<HTML>
+  <head>
+    <title>Patient Info</title>
+    <link rel="stylesheet" href="../styles.css">
+  </head>
+  <body>
+  <h1>Patient Admission</h1>
+<table>
+  <tr>
+      <th>Patient ID</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Group</th>
+      <th>Date Admitted</th>
 
-Patient info
+  </tr>
+<?php
+
+  $sql = "SELECT user.user_id, user.fname, user.lname FROM user, patient WHERE user.user_id = patient.user_id AND approved = 1 AND user.role_name='Patient' AND patient.group_number IS NULL";
+  $result = $conn->query($sql);
+
+  if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    ?><tr>
+          <?php echo "<td>" . $row['user_id'] . "</td>"?>
+          <?php echo "<td>" . $row["fname"] . "</td>"?>
+          <?php echo "<td>" . $row["lname"] . "</td>"?>
+          <td>
+            <form action='' method='post' name='info'>
+              <?php echo"<input type='hidden' name='user_id' value=" . $row['user_id'] . ">";?>
+              <select name='group_number'>
+                <option value='1'>1</option>
+                <option value='2'>2</option>
+                <option value='3'>3</option>
+                <option value='4'>4</option>
+              </select>
+          </td>
+          <td>
+            <input type="date" name="admit_date" id="admit_date">
+          </td>
+          <td>
+            <input type='submit' name='submit' value='Submit'>
+          </form>
+        </td>
+
+        </tr>
+<?php }
+} else {
+  echo "<h2>All Patient Info Up To Date</h2>";
+}
+?>
+</table>
+
+</body>
+</html>
--- admin/new-roster.php
+++ admin/new-roster.php
@@ -30,20 +30,21 @@ $conn = new mysqli($servername, $username, $password, $db);
 <?php
 if (isset($_POST['submit'])) {
 $date = $_POST['date'];
-$Supervisor = $_POST['Supervisor'];
-$Doctor = $_POST['Doctor'];
-$Caregiver1 = $_POST['Caregiver1'];
-$Caregiver2 = $_POST['Caregiver2'];
-$Caregiver3 = $_POST['Caregiver3'];
-$Caregiver4 = $_POST['Caregiver4'];
-$sql = "INSERT INTO roster (date ,supervisor_id, dr_id, caregiver1_id,caregiver2_id,caregiver3_id,caregiver4_id) VALUES ('$date', '$Supervisor', '$Doctor', '$Caregiver1', '$Caregiver2', '$Caregiver3', '$Caregiver4')";
-
-
-if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
+$supervisor = $_POST['supervisor'];
+$doctor = $_POST['doctor'];
+$caregiver1 = $_POST['caregiver1'];
+$caregiver2 = $_POST['caregiver2'];
+$caregiver3 = $_POST['caregiver3'];
+$caregiver4 = $_POST['caregiver4'];
+$sql = "INSERT INTO roster (date, supervisor_id, dr_id, caregiver1_id, caregiver2_id, caregiver3_id, caregiver4_id)
+        VALUES('$date', '$supervisor', '$doctor', '$caregiver1', '$caregiver2', '$caregiver3', '$caregiver4')";
+$result = $conn->query($sql);
+if ($result == TRUE) {
+  echo 'New roster created for ' . $date;
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
+
 }
 ?>
 <body>
@@ -53,124 +54,136 @@ if ($conn->query($sql) === TRUE) {
 <div class="new-roster">
 <div>
   <form action="" method="post" class="">
-    <label for="date"><b>Date of Birth</b></label>
-    <input type="date" name="date" id="date"><br><br>
-    <label for="Supervisor"><b>Select Supervisor</b></label>
+    <label for="date"><b>Date of Roster</b></label>
+    <input type="date" name="date" id="date" required><br><br>
+    <label for="supervisor"><b>Select Supervisor</b></label>
     <?php
-    $query = "SELECT fname, lname FROM user WHERE role_name = 'Supervisor'";
+    $query = "SELECT user_id, CONCAT(fname, ' ', lname) AS full_name FROM user WHERE role_name = 'Supervisor' AND approved=1";
     $Supervisor = $conn->query($query);
     $arr = [];
     if ($Supervisor->num_rows > 0) {
     // output data of each row
     while($row = $Supervisor->fetch_assoc()) {
-      array_push($arr, $row['fname']);
+      $arr[$row['user_id']] = $row['full_name'];
     }
   }
     ?>
-    <select id="supervisor" name="Supervisor" required>
+    <select id="supervisor" name="supervisor" required>
+      <option value=''>Select a Supervisor</option>
       <?php
-        foreach($arr as $r) { ?>
-          <option><?php echo $r; ?> </option>
-      <?php } ?>
+        foreach($arr as $r => $r_value) {
+          echo "<option value='$r'>$r_value</option>";
+        }
+       ?>
     </select><br><br>
 
-    <label for="Doctor"><b>Select Doctor</b></label>
+    <label for="doctor"><b>Select Doctor</b></label>
     <?php
-    $query = "SELECT fname, lname FROM user WHERE role_name = 'Doctor'";
+    $query = "SELECT user_id, CONCAT(fname, ' ', lname) as full_name FROM user WHERE role_name = 'Doctor' AND approved=1";
     $Doctor = $conn->query($query);
     $arr = [];
     if ($Doctor->num_rows > 0) {
     // output data of each row
     while($row = $Doctor->fetch_assoc()) {
-      array_push($arr, $row['fname']);
+      $arr[$row['user_id']] = $row['full_name'];
     }
   }
     ?>
-    <select id="doctor" name="Doctor" required>
+    <select id="doctor" name="doctor" required>
+      <option value=''>Select a Doctor</option>
       <?php
-        foreach($arr as $r) { ?>
-          <option><?php echo $r; ?> </option>
-      <?php } ?>
+        foreach($arr as $r => $r_value) {
+          echo "<option value='$r'>$r_value</option>";
+        }
+       ?>
     </select><br><br>
 
-    <label for="Caregiver1"><b>Select Caregiver1</b></label>
+    <label for="caregiver1"><b>Select Caregiver 1</b></label>
     <?php
-    $query = "SELECT fname, lname FROM user WHERE role_name = 'Caregiver'";
+    $query = "SELECT user_id, CONCAT(fname, ' ', lname) as full_name FROM user WHERE role_name = 'Caregiver' AND approved=1";
     $Caregiver1 = $conn->query($query);
     $arr = [];
     if ($Caregiver1->num_rows > 0) {
     // output data of each row
     while($row = $Caregiver1->fetch_assoc()) {
-      array_push($arr, $row['fname']);
+      $arr[$row['user_id']] = $row['full_name'];
     }
   }
     ?>
-    <select id="caregiver1" name="Caregiver1" required>
+    <select id="caregiver1" name="caregiver1" required>
+      <option value=''>Select a Caregiver</option>
       <?php
-        foreach($arr as $r) { ?>
-          <option><?php echo $r; ?> </option>
-      <?php } ?>
+        foreach($arr as $r => $r_value) {
+          echo "<option value='$r'>$r_value</option>";
+        }
+       ?>
     </select><br><br>
 
-    <label for="Caregiver2"><b>Select Caregiver2</b></label>
+    <label for="caregiver2"><b>Select Caregiver 2</b></label>
     <?php
-    $query = "SELECT fname, lname FROM user WHERE role_name = 'Caregiver'";
+    $query = "SELECT user_id, CONCAT(fname, ' ', lname) as full_name FROM user WHERE role_name = 'Caregiver' AND approved=1";
     $Caregiver2 = $conn->query($query);
     $arr = [];
     if ($Caregiver2->num_rows > 0) {
     // output data of each row
     while($row = $Caregiver2->fetch_assoc()) {
-      array_push($arr, $row['fname']);
+      $arr[$row['user_id']] = $row['full_name'];
     }
   }
     ?>
-    <select id="caregiver2" name="Caregiver2" required>
+    <select id="caregiver2" name="caregiver2" required>
+      <option value=''>Select a Caregiver</option>
       <?php
-        foreach($arr as $r) { ?>
-          <option><?php echo $r; ?> </option>
-      <?php } ?>
+        foreach($arr as $r => $r_value) {
+          echo "<option value='$r'>$r_value</option>";
+        }
+       ?>
     </select><br><br>
 
 
 
-    <label for="Caregiver3"><b>Select Caregiver3</b></label>
+    <label for="caregiver3"><b>Select Caregiver 3</b></label>
     <?php
-    $query = "SELECT fname, lname FROM user WHERE role_name = 'Caregiver'";
+    $query = "SELECT user_id, CONCAT(fname, ' ', lname) as full_name FROM user WHERE role_name = 'Caregiver' AND approved=1";
     $Caregiver3 = $conn->query($query);
     $arr = [];
     if ($Caregiver3->num_rows > 0) {
     // output data of each row
     while($row = $Caregiver3->fetch_assoc()) {
-      array_push($arr, $row['fname']);
+      $arr[$row['user_id']] = $row['full_name'];
     }
   }
     ?>
-    <select id="caregiver3" name="Caregiver3" required>
+    <select id="caregiver3" name="caregiver3" required>
+      <option value=''>Select a Caregiver</option>
       <?php
-        foreach($arr as $r) { ?>
-          <option><?php echo $r; ?> </option>
-      <?php } ?>
+        foreach($arr as $r => $r_value) {
+          echo "<option value='$r'>$r_value</option>";
+        }
+       ?>
     </select><br><br>
 
 
 
-    <label for="Caregiver4"><b>Select Caregiver4</b></label>
+    <label for="caregiver4"><b>Select Caregiver 4</b></label>
     <?php
-    $query = "SELECT fname, lname FROM user WHERE role_name = 'Caregiver'";
+    $query = "SELECT user_id, CONCAT(fname, ' ', lname) as full_name FROM user WHERE role_name = 'Caregiver' AND approved=1";
     $Caregiver4 = $conn->query($query);
     $arr = [];
     if ($Caregiver4->num_rows > 0) {
     // output data of each row
     while($row = $Caregiver4->fetch_assoc()) {
-      array_push($arr, $row['fname']);
+      $arr[$row['user_id']] = $row['full_name'];
     }
   }
     ?>
-    <select id="caregiver4" name="Caregiver4" required>
+    <select id="caregiver4" name="caregiver4" required>
+      <option value=''>Select a Caregiver</option>
       <?php
-        foreach($arr as $r) { ?>
-          <option><?php echo $r; ?> </option>
-      <?php } ?>
+        foreach($arr as $r => $r_value) {
+          echo "<option value='$r'>$r_value</option>";
+        }
+       ?>
     </select><br><br>
 
 
