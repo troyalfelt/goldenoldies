5e94eeb5676d4e0ca77c0909a277ff5eb5785dfd
Troy Alfelt <troyalfelt@Troys-MacBook-Pro.local>
2021-12-02 08:20:44 -0500

    fixed roles page

--- admin/roles.php
+++ admin/roles.php
@@ -1,112 +1,77 @@
 <?php
-session_start();
-if (!isset($_SESSION['access_lvl'])) {
-  header("Location: ../login.php");
-} else {
-  if ($_SESSION['access_lvl'] !== '1') {
-    header("Location: ../login.php");
-}
-}
-?>
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+  ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>Approval</title>
-  <link rel="stylesheet" href="../styles.css">
+  <title>New Roles</title>
+  <link rel="stylesheet" type="text/css" href="../styles.css"/>
 </head>
+
 <?php
 $servername = "localhost";
 $username = "troyalfelt";
 $password = "";
 $db = 'test';
 $conn = new mysqli($servername, $username, $password, $db);
- ?>
+?>
+<?php if (isset($_POST['submit'])) {
+  $role = $_POST['role'];
+  $access = $_POST['access'];
+  $sql = "INSERT INTO roles (role_name, access_lvl) VALUES ('$role', '$access')";
 
- <?php
- if (isset($_POST['submit']))  {
-       $user_id = $_POST['user_id'];
-       if ($_POST['status'] == 'approved') {
-         $access_lvl = $_POST['access_lvl'];
-         $qry = "UPDATE user SET approved = 1 WHERE user_id= '$user_id'";
-         $rslt = $conn->query($qry);
-         if ($access_lvl <= 4) {
-           $qry2 = "INSERT INTO employee (user_id) VALUES ('$user_id')";
-           $rslt2 = $conn->query($qry2);
-           echo 'Employee Registered';
-         } elseif ($access_lvl == 5) {
-           $qry2 = "INSERT INTO patient (user_id) VALUES ('$user_id')";
-           $rslt2 = $conn->query($qry2);
-         } else {
-           $qry2 = "INSERT INTO family (user_id) VALUES ('$user_id')";
-           $rslt2 = $conn->query($qry2);
-         }
-         } else {
-           $qry = "DELETE FROM user WHERE user_id= '$user_id'";
-           $rslt = $conn->query($qry);
-           if ($rslt == TRUE) {
-             echo "User deleted";
-           } else {
-               echo "Error deleting record: " . $conn->error;
-           }
-         }
+
+  if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
 }
 ?>
+
 <body>
   <header>
       <h1>Golden Oldies</h1>
   </header>
   <div class="container">
-  <h2>Registration Approval</h2>
-  <table>
-    <tr>
-        <th>First Name</th>
-        <th>Last Name</th>
-        <th>Role</th>
-    </tr>
+    <table>
+      <tr>
+          <th>Role Name</th>
+          <th>Access Level</th>
 
-<?php
+      </tr>
 
-    $sql = "SELECT user.user_id, user.fname, user.lname, user.role_name, roles.access_lvl FROM user, roles WHERE approved = 0 AND user.role_name = roles.role_name";
-    $result = $conn->query($sql);
+  <?php
 
-    if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-      ?><tr>
-            <?php echo "<td>" . $row["fname"] . "</td>"?>
-            <?php echo "<td>" . $row["lname"] . "</td>"?>
-            <?php echo "<td>" . $row["role_name"] . "</td>"?>
-            <td>
-              <form action='' method='post' name='approved'>
-                <?php echo"<input type='hidden' name='user_id' value=" . $row['user_id'] . ">";?>
-                <?php echo"<input type='hidden' name='access_lvl' value=" . $row['access_lvl'] . ">";?>
-                <input type='hidden' name='status' value='approved'>
-                <input type='submit' name='submit' value='Approve'>
-                </form>
-            </td>
-            <td>
-              <form action='' method='post' name='disapproved'>
-                <?php echo"<input type='hidden' name='user_id' value=" . $row['user_id'] . ">";?>
-                <input type='hidden' name='status' value='disapproved'>
-              <input type='submit' name='submit' value='Disapprove'>
-              </form>
-            </td>
-          </tr>
-<?php }
-  }
-  ?>
-</table>
-        </div>
-        <footer>
-        <script>  if ( window.history.replaceState ) {
-  window.history.replaceState( null, null, window.location.href );
-}
-</script>
-            <h3>Contact Us</h3>
-            <p>000-000-0000</p>
-        </footer>
-        </body>
-        </html>
+      $sql = "SELECT role_name, access_lvl FROM roles ORDER BY access_lvl ASC";
+      $result = $conn->query($sql);
+
+      if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+        ?><tr>
+              <?php echo "<td>" . $row["role_name"] . "</td>"?>
+              <?php echo "<td>" . $row["access_lvl"] . "</td>"?>
+            </tr>
+  <?php }
+    }
+    ?>
+  </table>
+    <form action="" method="post" class="">
+        <label for="role"><b>New Role</b></label><br>
+        <input type="text" placeholder="Enter Role" name="role" id="role" required><br>
+        <label for="accsess"><b>Access Level</b></label><br>
+        <input type="text" placeholder="Enter Access Level" name="access" id="access" required><br>
+        <input type="submit" class="btn" name="submit" value="Okay">
+    </form>
+  </div>
+<footer>
+    <h3>Contact Us</h3>
+    <p>000-000-0000</p>
+</footer>
+</body>
+</html>
