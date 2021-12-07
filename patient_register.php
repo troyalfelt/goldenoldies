c285831de88c5ff6a55c3f5f8c3507dba512f048
Michael Baker <michaelbaker117@gmail.com>
2021-12-07 08:48:52 -0500

    CSS Update

--- patient_register.php
+++ patient_register.php
@@ -69,36 +69,28 @@ error_reporting(E_ALL);
     <div class="max-w-md w-full space-y-8">
       <div>
         <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Register for an account
+          Patient Registration
         </h2>
       </div>
-      <div><p>If you're not a patient, register <a href="register.php" class="text-yellow-400 hover:bg-gray-200">here.</a></p></div>
+
       <form class="mt-8 space-y-6" action="" method="POST">
           <?php echo"<input type='hidden' name='user_id' value=" . $temp_id . ">";?>
         <div class="rounded-md shadow-sm -space-y-px">
           <div>
-            <label for="fName" class="sr-only">First Name</label>
-            <input id="fname" name="fname" type="text" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Enter First Name" required>
+            <label for="fCode" class="sr-only">Family Code</label>
+            <input id="fCode" name="fCode" type="text" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Enter Family Code" required>
           </div>
           <div>
-            <label for="lName" class="sr-only">Last Name</label>
-            <input id="lname" name="lname" type="text" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Enter Last Name" required><br>
+            <label for="eContact" class="sr-only">Emergency Contact</label>
+            <input id="eContact" name="eContact" type="text" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Enter Emergency Contact" required><br>
           </div>
           <div>
-            <label for="dob" class="sr-only">Date of Birth</label>
-            <input id="dob" name="dob" type="text" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Date of Birth: mm/dd/yyyy" required>
+            <label for="ePhone" class="sr-only">Emergency Contact Number</label>
+            <input id="ePhone" name="ePhone" type="text" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Emergency Contact Number" required>
           </div>
           <div>
-            <label for="phone" class="sr-only">Phone Number</label>
-            <input id="phone" name="phone" type="text" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Enter Phone Number" required><br>
-          </div>
-          <div>
-            <label for="email" class="sr-only">Email address</label>
-            <input id="email" name="email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address">
-          </div>
-          <div>
-            <label for="password" class="sr-only">Password</label>
-            <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password">
+            <label for="relation" class="sr-only">Relation to Emergency Contact</label>
+            <input id="relation" name="relation" type="text" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Relation to Emergency Contact" required><br>
           </div>
         </div>
         <p>Already have an account? <a href="login.php" class="text-yellow-400 hover:bg-gray-200">Login here.</a></p>
@@ -110,25 +102,6 @@ error_reporting(E_ALL);
         </div>
       </form>
     </div>
-  </div>
-
-
-  <div class="container">
-    <form action='' method='post' class="f-register">
-        
-        <div class="patient">
-            <h1>Patient Registration</h1>
-            <label for="fCode"><b>Family Code</b></label>
-            <input type="text" placeholder="Enter Code" name="fCode" id="fCode"><br>
-            <label for="eContact"><b>Emergency Contact</b></label>
-            <input type="text" placeholder="Enter Contact Name" name="eContact" id="eContact"><br>
-            <label for="ePhone"><b>Emergency Contact Number</b></label>
-            <input type="text" placeholder="Enter Contact Phone Number" name="ePhone" id="ePhone"><br>
-            <label for="relation"><b>Relation to Emergency Contact</b></label>
-            <input type="text" placeholder="Enter Relation" name="relation" id="relation"><br>
-            <input type="submit" class="btn" name="submit" value="Register">
-        </div>
-    </form>
   </div>
 </body>
 </html>
